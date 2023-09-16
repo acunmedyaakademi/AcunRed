d21ed801320e230a48b3c90ce9275f4f4f85ea38
sisyphos55 <benikayitet@yandex.com>
2023-09-16 17:16:07 +0300

    test

--- AcunRedMain/api/views.py
+++ AcunRedMain/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
 # Create your views here.
+
+class LoginViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+
+    def login(self, request):
+        # Get the username and password from the request
+        username = request.data['username']
+        password = request.data['password']
+
+        # Authenticate the user
+        user = authenticate(username=username, password=password)
+
+        # If the user is authenticated, return a success response
+        if user is not None:
+            return Response({'success': True, 'user': user.username})
+
+        # If the user is not authenticated, return a failure response
+        else:
+            return Response({'success': False, 'error': 'Invalid username or password'})
